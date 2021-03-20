633e8656cf25410271d8efddb78df13542ccc863
Adrian <adrian.villamayor@outlook.com>
2021-03-20 18:20:01 +0100

    File system

--- test8.py
+++ test8.py
@@ -14,14 +14,49 @@ def exportLista(lista):
     if len(lista) > 0:
         with open('compra.txt', 'w') as f:
             for x in lista:
-                f.write("- {}, {} unidad \n".format(x[0], x[1]))
+                f.write("- {}, {} u. \n".format(x[0], x[1]))
     else:
         print("La lista esta vacia.")
 
 
+def prepareList(lista_encode):
+    lista_importada = []
+
+    for x in lista_encode:
+        x = x.replace(" ", "")
+        x = x.replace("-", "")
+        order = x.split(",")
+
+        if order[0] != '':
+            producto = order[0]
+            cantidad = order[1].replace("u.", "")
+            lista_importada.append([producto, cantidad])
+
+    return lista_importada
+
+
+def importLista(lista):
+    lista = []
+    if (input("Quieres exportar alguna lista que este en el mismo root (Y/N) ? \n"
+              " - ")).upper() == "Y":
+        try :
+
+            with open('compra.txt', 'r') as f:
+                line = f.read().split("\n")
+                lista = prepareList(line)
+
+            print("Lista importada con exito \n")
+        except FileNotFoundError:
+            print("El archivo compra.txt no existe.")
+
+        return lista
+
 def main():
     lista = []
     orden = ""
+
+    lista = importLista(lista)
+    print(lista)
 
     while orden == "" or orden != "Nada":
         orden = generarOrdern()
